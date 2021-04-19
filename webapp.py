a3a8c818e61f553ea1c0592da97612ad8c332713
Ponmo <77691125+Ponmo@users.noreply.github.com>
2021-04-19 09:41:53 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -8,7 +8,7 @@ app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  
 def render_main():
     return render_template('home.html', response = get_state_options())
 
-@app.route("/response", methods=['GET', 'POST']))
+@app.route("/response", methods=['GET', 'POST'])
 def render_response():
     state_selected = request.form['states']
     if request.method() == 'POST':
